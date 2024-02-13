fbcf69c18bbb473e92212ac9f796819298e99f38
Jack Sheehy <jackhughsheehy@gmail.com>
2024-02-13 22:41:08 +0000

    Merge fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Poppins } from "next/font/google";
-<<<<<<< HEAD
 import Footer from "./footer";
-=======
-import Navbar from "./navbar";
->>>>>>> 3d45c73b348c8e0b64496aab90cc0782bd4a3de7
 
 const poppins = Poppins({
   subsets: ["latin"],
