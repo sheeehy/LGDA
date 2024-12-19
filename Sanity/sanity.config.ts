5185e960af8e14148903eccd93380648efca4998
Jack Sheehy <jackhughsheehy@gmail.com>
2024-12-19 03:26:35 +0000

    Remove vision plugin

--- Sanity/sanity.config.ts
+++ Sanity/sanity.config.ts
@@ -1,6 +1,5 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
@@ -11,7 +10,6 @@ export default defineConfig({
   projectId: 'pbg2oc94',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
