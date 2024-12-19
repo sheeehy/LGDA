58fbcaccec05b93a44bd03050d1c61f20be835a9
Jack Sheehy <jackhughsheehy@gmail.com>
2024-12-19 05:11:01 +0000

    Remove Button

--- src/components/Archive.tsx
+++ src/components/Archive.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { getArchiveData } from "@/app/lib/sanity";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
@@ -16,10 +15,12 @@ export async function Archive() {
     <ScrollArea className="h-[300px] w-full rounded-md border p-4">
       <div className="grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
         {archiveFiles.map((file: ArchiveFile) => (
-          <Button key={file.year} variant="outline" className="w-full transition-colors duration-300 hover:bg-teal-50 hover:text-teal-700 hover:border-teal-500" onClick={() => {}}>
-            <FileText className="h-4 w-4 mr-2" />
-            LGDA {file.year}
-          </Button>
+          <a key={file.year} href={file.url} target="_blank" rel="noopener noreferrer">
+            <Button variant="outline" className="w-full transition-colors duration-300 hover:bg-teal-50 hover:text-teal-700 hover:border-teal-500">
+              <FileText className="h-4 w-4 mr-2" />
+              LGDA {file.year}
+            </Button>
+          </a>
         ))}
       </div>
     </ScrollArea>
